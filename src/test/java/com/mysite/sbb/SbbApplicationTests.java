7cfef69901a22cbae6c971c469eb43325a99dbbe
socra167 <socra167@gmail.com>
2025-01-13 16:38:11 +0900

    :wrench: config: 테스트 시 test 데이터소스를 사용하도록 변경

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,12 +1,18 @@
 package com.mysite.sbb;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource("classpath:application-test.properties")
 @SpringBootTest
 class SbbApplicationTests {
 	@Autowired
@@ -16,19 +22,38 @@ class SbbApplicationTests {
 	void contextLoads() {
 	}
 
-	@Test
-	@DisplayName("질문을 저장할 수 있다.")
-	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
+	@Nested
+	@DisplayName("데이터베이스 테스트")
+	class databaseTest {
 
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		@Test
+		@Order(1)
+		@DisplayName("질문을 저장할 수 있다.")
+		void saveQuestion() {
+			questionRepository.deleteAll();
+
+			var q1 = new Question();
+			q1.setSubject("sbb가 무엇인가요?");
+			q1.setContent("sbb에 대해서 알고 싶습니다.");
+			q1.setCreateDate(LocalDateTime.now());
+			questionRepository.save(q1);
+
+			var q2 = new Question();
+			q2.setSubject("스프링부트 모델 질문입니다.");
+			q2.setContent("id는 자동으로 생성되나요?");
+			q2.setCreateDate(LocalDateTime.now());
+			questionRepository.save(q2);
+		}
+
+		@Test
+		@Order(2)
+		@DisplayName("저장된 질문을 모두 조회할 수 있다.")
+		void findAllQuesitons() {
+			var allQuestions = questionRepository.findAll();
+			assertThat(allQuestions.size()).isEqualTo(2);
+
+			var q = allQuestions.get(0);
+			assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
+		}
 	}
 }
