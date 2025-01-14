76b4363fa87a9b7e07748edd4bbb2a9be63a937a
socra167 <socra167@gmail.com>
2025-01-14 19:23:53 +0900

    :sparkles: feat: Subject로 Question 검색

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,8 +1,12 @@
 package com.mysite.sbb;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+	Optional<Question> findBySubject(String subject);
 }
