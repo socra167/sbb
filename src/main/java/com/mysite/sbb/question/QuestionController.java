82c536574e5f2340942cf2c4cd2e4464c7240c45
socra167 <socra167@gmail.com>
2025-01-16 11:39:27 +0900

    :sparkles: feat: 입력 값 검증

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -33,14 +34,17 @@ public class QuestionController {
 	}
 
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam(value = "subject") String subject,
-		@RequestParam(value = "content") String content) {
-		questionService.create(subject, content);
+	public String questionCreate(@Valid QuestionForm questionForm,
+		BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
 	}
 }
