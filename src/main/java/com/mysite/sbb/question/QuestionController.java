2d1ff03a0184ace6f6abc6f2c42ddf13c28f5aef
socra167 <socra167@gmail.com>
2025-01-16 09:58:11 +0900

    :sparkles: feat: 질문 상세 페이지 연결

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,5 +20,11 @@ public class QuestionController {
 		List<Question> questionList = questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+
+	@GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		model.addAttribute("question", questionService.getQuestion(id));
+		return "question_detail";
 	}
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.mysite.sbb.DataNotFoundException;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -14,5 +16,9 @@ public class QuestionService {
 
 	public List<Question> getList() {
 		return questionRepository.findAll();
+	}
+
+	public Question getQuestion(final Integer id) {
+		return questionRepository.findById(id).orElseThrow(() -> new DataNotFoundException("question not found"));
 	}
 }
