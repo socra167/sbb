3608a05d81ee9b99e709d7c7b46126fa840343af
socra167 <socra167@gmail.com>
2025-02-04 17:12:49 +0900

    :recycle: refactor: `PasswordEncoder`로 `BCryptPasswordEncoder` 빈을 등록해 사용하도록 변경

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -15,6 +17,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration // 스프링의 환경 설정 파일
 @EnableWebSecurity // 모든 요청 URL이 스프링 시큐리티의 제어를 받게 함 (스프링 시큐리티 활성화)
 public class SecurityConfig {
+
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		// "/**" 인증되지 않은 모든 페이지의 요청을 허락한다, csrf 처리 시 "/h2-console/**"는 예외로 처리한다
@@ -26,5 +29,10 @@ public class SecurityConfig {
 				.addHeaderWriter(new XFrameOptionsHeaderWriter(
 					XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
 		return http.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
