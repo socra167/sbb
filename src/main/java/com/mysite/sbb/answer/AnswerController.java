35fb60661b13d3ff751526591681619a56bb8351
socra167 <socra167@gmail.com>
2025-02-13 17:47:59 +0900

    :sparkles: feat: 답변 수정 기능 구현

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,23 +2,26 @@ package com.mysite.sbb.answer;
 
 import java.security.Principal;
 
-import jakarta.validation.Valid;
+import javax.naming.Binding;
 
-import org.aspectj.weaver.tools.cache.AbstractIndexedFileCacheBacking;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
 import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.user.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -41,5 +44,24 @@ public class AnswerController {
 		}
 		answerService.create(question, answerForm.getContent(), siteUser);
 		return String.format("redirect:/question/detail/%s", id);
+	}
+
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/modify/{id}")
+	public String answerModify(
+		AnswerForm answerForm,
+		BindingResult bindingResult,
+		@PathVariable Integer id,
+		Principal principal
+	) {
+		if (bindingResult.hasErrors()) {
+			return "answer_form";
+		}
+		Answer answer = answerService.getAnswer(id);
+		if (answer.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+		}
+		answerService.modify(answer, answerForm.getContent());
+		return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
 	}
 }
